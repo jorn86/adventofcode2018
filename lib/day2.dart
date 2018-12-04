40faa4de14a71caef2be7cb9c905b756b7e9b0e9
JornH <j.hertsig@topdesk.com>
2018-12-04 18:28:14 +0100

    First four days

---
+++ lib/day2.dart
@@ -0,0 +1,303 @@
+const input = '''ivyhczwokexltwhsfamqprbnuy
+ivjhcjdokexltwwsfamqpabnuy
+ivjhczdokebltwgsfydqprbnuy
+ivjhcadokexftogsfamqprbnuy
+idjhczdokexltwgsfacqprbnuh
+ivjhgzookexltwgsfamqjrbnuy
+uvjhctdokexltwosfamqprbnuy
+ivrhczdokexltwhzfamqprbnuy
+ivjhczuxkexltwgsfamqprbney
+ivjhczdokemltwgsfadnprbnuy
+ifjhczdokexltwgsfamqprbkuf
+ivjhkzdokltltwgsfamqprbnuy
+ivjuczdhkexltwgsfamqprtnuy
+ivjhjzdxkexltwgsfapqprbnuy
+ivlhczdoxexltwgsfamqprgnuy
+ivjhczdoknxltwgssamqsrbnuy
+ivjhczdokexltwgswadqprbruy
+ivjhczdokexthwgsfampprbnuy
+uvjhczrozexltwgsfamqprbnuy
+ivolczdokexltwgsffmqprbnuy
+ivjhczibkexltwgsfamoprbnuy
+ivjhczdokefltmgsfamqprbouy
+ivjhczdobexltngsfamsprbnuy
+ivjhczdokexltwvsfxqqprbnuy
+dvjhczdokexucwgsfamqprbnuy
+kvjhszkokexltwgsfamqprbnuy
+ivjhczdokexrtegsfamqprbnus
+ivjhctdokexltwglgamqprbnuy
+ivjhczdozexutwgsfamqdrbnuy
+ivjhczqokemltwgsfakqprbnuy
+uvjhczdokexlqwgsfadqprbnuy
+ivjhczdohexltwglffmqprbnuy
+izjhczdokexltwgsfamqprbsqy
+iajhczdokwxltwgjfamqprbnuy
+ivjfczdokexllwgslamqprbnuy
+ivjhczdoyexltwgsfamqdrbnxy
+ivjhczdokekwtwssfamqprbnuy
+ivjhcodokexltwgsfamqirxnuy
+ihjhhzdokexltwgsfamqlrbnuy
+ivjdpzdokexltwfsfamqprbnuy
+ivjhcpdokexltwgsfamqqrbruy
+qvjhcziokexltwgsfamqprbnny
+ivohczdomexltwgsfkmqprbnuy
+ivjhczhokhxlywgsfamqprbnuy
+ivjhczdokexltwgmffmqprbruy
+ivjhczdokqxltwgcfamqprbnyy
+ivjhczdokepltwgsfamcprbnay
+ivjhczdokexltwgseamqpmbnua
+ivjzczdokexltwgszamqplbnuy
+ivjhczpokexltwgvfgmqprbnuy
+idjhczdokexltwgsxamqprbndy
+ivjhczdxkexltwgcgamqprbnuy
+ivjhczdckexatpgsfamqprbnuy
+ivjrczdorexltwgsfamqprbnvy
+ivjoczdokexltwgswamqprbtuy
+iylhczdokexltwgsfamqpxbnuy
+imxhczdokkxltwgsfamqprbnuy
+ivvhczdoktxltwgsfamaprbnuy
+ivyhczdokexltwhsfayqprbnuy
+ivjhcrdokexltegsfamqprbnum
+rvjhezdokexltwgsoamqprbnuy
+ivjzczdokexlbwgsfkmqprbnuy
+ivjhczdokelltwgsyamqprbnoy
+ixjhczdorexltwgsfamqprbuuy
+ivjhczpokexdtwglfamqprbnuy
+ivjhczdokexltwgfgamcprbnuy
+ikjhczdokexltwgsfamqirbnux
+ivjhczdopjxltwgsfamqprbnny
+ivchczdokexltwgniamqprbnuy
+ivjhczdooeqltwgsfamqprbniy
+ivjhcldonexltwgbfamqprbnuy
+ixjhczdokehltwgsfamqprbnuf
+ivjhczdckefltwgsfamqppbnuy
+ivjhczdoqrxltwgsfamqprbnun
+ivjhczdokcxltwgmfarqprbnuy
+ivjhcziorexltqgsfamqprbnuy
+ivjhwzdokexltwgnfamqprbcuy
+ivjhczdoqexltwgsfazqprunuy
+iijhczdokexltwgsyamqprbnug
+ivjhczdokexltwgxfamhprbnry
+ivjhczdakexltwgsfaeqlrbnuy
+ivjhqzdokehltwgsfampprbnuy
+ivjhczdokexltwlsfpmyprbnuy
+ivjhfzdoktxltwgsfamzprbnuy
+ivlhvzdokexltwgsvamqprbnuy
+ivjhczdbkexltwgsaamqprfnuy
+ivahcedokexltigsfamqprbnuy
+cvjhczdokexltwgsfamapibnuy
+ivjhczkokbxltwgsfbmqprbnuy
+pvjuczdnkexltwgsfamqprbnuy
+iyjhczdckexotwgsfamqprbnuy
+ivjhzzdokvxltwgsfamqprbnuo
+ivjhczdobexltwgsxamqprbnry
+ivjhczdokexltwgsfaprprbnub
+ivjhczdokexltwgofarqprbkuy
+ivjhczdokexltwgbfymqprbnhy
+ibjhczdokexltwgsfkmqpvbnuy
+ivjhczdzkexlywgsfacqprbnuy
+hvdhczdokexltwglfamqprbnuy
+ivjhczdokexrtwgsfamqprbsuh
+ivjhczhokexltngsfamqpjbnuy
+ivjhcsjokexltwgsfaeqprbnuy
+ivjmczdokexltmgsfamqpbbnuy
+wvjhczdokexltwgsfamkpkbnuy
+icjhpzdoaexltwgsfamqprbnuy
+ivjmczdhkexltwgsfzmqprbnuy
+ivjhczdokexytwgsfamqprbwug
+ikjhczdjkexljwgsfamqprbnuy
+ivjvcdmokexltwgsfamqprbnuy
+ivjhazdorixltwgsfamqprbnuy
+ivchczdokexltwgsfamzprenuy
+ivjcczdokexlttgsfamqpmbnuy
+ibchgzdokexltwgsfamqprbnuy
+ivjhczdokepltwgsfamqpeenuy
+ivjnwzdokexlrwgsfamqprbnuy
+ivjhczdokexitwgsfadqlrbnuy
+icjhcrdokexltwgsfamqkrbnuy
+ivngczdokexltwgsfamqprbyuy
+ivjhuudokexlvwgsfamqprbnuy
+ivjhczdnkexltwgsfhmqpxbnuy
+itjhczdokexltwvsfamgprbnuy
+ivjhcddokexltwgsfakqprbnny
+ivjhuzdojexltwfsfamqprbnuy
+idjhczdokexltwgsfamqukbnuy
+ivjhczdokexlzigsfamqprbngy
+ivjwczdokexltwgufamqprbnuo
+iijhczdokexltwfsfadqprbnuy
+ivjhczdukexdtwgsfamqpsbnuy
+idjhczdokexllwgssamqprbnuy
+zvjhczdokexrtwgsfamqplbnuy
+ivphczdofexltwgefamqprbnuy
+ivhhczdokexlpwgsjamqprbnuy
+ivjhczdovexltwgsfamqprhnuj
+ivjhczdoklxltwgseamqprlnuy
+ivjhcqdokexltngsfamqprdnuy
+ivjhczdoifxltagsfamqprbnuy
+izjhczdokexltwjsramqprbnuy
+psjhczdokexlgwgsfamqprbnuy
+ivjhcadokexltwgsfsmqwrbnuy
+ivjhczdokexltwgsfawqiibnuy
+ivjhczkokexhtwgsfamqprbnuk
+ivjhcmdukexltwgsfamvprbnuy
+ivjlczdokexltwgsfamquibnuy
+ivjhczdokexntwgyfamqprbniy
+ivjhczdokexltwlsfafqprbnuc
+ivjhczdosexltrtsfamqprbnuy
+ivjhcznokexbtwgsfafqprbnuy
+ivwtczdotexltwgsfamqprbnuy
+ivjhvzdokexltigsoamqprbnuy
+ivjhcmdokexltwasfamqirbnuy
+ivthczdokexltwgsfaydprbnuy
+ivjhwzdskexltwgsfamqprbnus
+icjhczdosuxltwgsfamqprbnuy
+ivjhczdokexltwgstamqbrmnuy
+iejhczuoktxltwgsfamqprbnuy
+ivjhczdokeqltwgskamqprbniy
+ivjhlzdokexltugsfamqprbpuy
+iwjqczdckexltwgsfamqprbnuy
+ivjhwzdokexluwgsfxmqprbnuy
+ivjhczdokexltwgwfwmqprbguy
+gvjhczkokexltwgsfgmqprbnuy
+ivjhczdoyexlhwgsfamqprbnoy
+cvjhczdokexltwgsfomqprinuy
+vvmhczdokexltwgsfamqprbnun
+vvjhczdokexltwgsftmfprbnuy
+ivkhckdokhxltwgsfamqprbnuy
+iyjhczdkkexltjgsfamqprbnuy
+ivlhczdokexltwgsfamqyrbhuy
+tvjhmzdokexltwgsfamqorbnuy
+ivjhczdokexltwvsfamqprbnxi
+ivjhczdowexltwgswamqerbnuy
+wvjiczdomexltwgsfamqprbnuy
+ivjpizdokexltwgvfamqprbnuy
+ivjhuzdokexlzwgspamqprbnuy
+ivjhczdokeyltwgkfamqprdnuy
+jvjhczdokexlnwgsfamqirbnuy
+ivjheidokexltwvsfamqprbnuy
+mvjhczdokexltwgsfamqyrsnuy
+ivjhazdykexltwgsramqprbnuy
+ivjkcodokexltwgsxamqprbnuy
+ikjhczdoktxltwgpfamqprbnuy
+ivjhyzdfkexmtwgsfamqprbnuy
+ivohczdokexltugsfamqprynuy
+ivjkczdqkexltwgshamqprbnuy
+ivjhczdokexltwgskamqynbnuy
+icjhczdokexltwgofamrprbnuy
+ivjhlzdokealtwgsfamqsrbnuy
+ivehczdybexltwgsfamqprbnuy
+ovjhczdokexltwgsfamqirbnuo
+ivjoczdokexltwgsfamqurbnty
+ivjmczdokexltwgsfrmqprnnuy
+ivjhczdowpxltwgbfamqprbnuy
+ivjhczdokexltwfsfamqkrgnuy
+ivjhwzdokexltwgsfavqprbnuq
+jvjhczdokexltwgsiamqprbnny
+ivjhlzdouexltwfsfamqprbnuy
+ivjhczdokexltwgsfamqbrbnlv
+iwjhczdokexltwgsfapqprbnqy
+idjhczdokexltwgsaamqrrbnuy
+ivjhjzdopepltwgsfamqprbnuy
+ivjmczdokejltwgsfamqpbbnuy
+ivjhczdokexltwgsuamdprvnuy
+injhczdokexltwgefamqurbnuy
+iujhczdokexltwgsaamqjrbnuy
+ivjhczdokexltwgvfaaqprbnly
+ivehczdokexltwgsfamqppbnui
+ivxhczdodexltwgsfamqplbnuy
+ivjhczfokexltwgsfamqpwbauy
+ivjhcztwkexhtwgsfamqprbnuy
+ivjeczdokexltygsfmmqprbnuy
+ivjhchdokexltwgsmameprbnuy
+ivkhczdoklxltwggfamqprbnuy
+ivjhczdzkexltwhsfamqprjnuy
+ivjhcedokeultngsfamqprbnuy
+ivjhczdokexvtwgseabqprbnuy
+ivjhczdooexltlgsfamqpibnuy
+ivjgczvosexltwgsfamqprbnuy
+ivlhczwokexltwgsfamqmrbnuy
+lvjhczdokexutwgsfamrprbnuy
+ivahczdokexpdwgsfamqprbnuy
+ivjhcznokexltwhsfamqpnbnuy
+ivjhczdpkyxltwgbfamqprbnuy
+ivjhnzdokexltwgsftmqprinuy
+ivihczdokexltnhsfamqprbnuy
+ivjhcbdokevltwgsfamqprbauy
+hgjoczdokexltwgsfamqprbnuy
+dvjhczdckexltwgsfamqpybnuy
+ivjhcadokesltwgsfsmqwrbnuy
+ivjhwzdokexlttgsfamqprbney
+ivjhcidokexltwgofamqfrbnuy
+ivokwzdokexltwgsfamqprbnuy
+ivjiczdokexltwgsfaqqarbnuy
+ivjhczdokexqtwfsfamgprbnuy
+ivjhczdokealtwgsfamqerbnqy
+ivjhczdskexltwgsfamqprznuu
+ivjhwzdokexltwjsfdmqprbnuy
+ivjhczaokexlzwgsfamqprbnus
+ivjhczdokexltwosfamqnrbnux
+ivjhczdokexlqwgsfamwprcnuy
+ivjhczdqkexltwgswamqpcbnuy
+ijjhczdokexnttgsfamqprbnuy
+ivjhcedckexltwgsfamqprbnpy
+ivjhczdokeyltwgsfamqshbnuy
+ivjhczdokexltsgsfamqpmznuy
+ivjlczdtkeiltwgsfamqprbnuy
+ivjhczdokexltwgsfkmtprbnby
+ivjhnzdozexltwgsfamqprbnuc
+xqjxczdokexltwgsfamqprbnuy
+ivjhczdokeyltwgsfamqnrbnuw
+ivjwczgokexltwgsfamvprbnuy
+''';
+
+void first() {
+  var ids = input.split('\n');
+  var twos = 0;
+  var threes = 0;
+  for (var value in ids) {
+    var chars = <int, int>{};
+    value.runes.forEach((c) {
+      if (chars.containsKey(c)) {
+        chars[c] =  chars[c] + 1;
+      }
+      else {
+        chars[c] = 1;
+      }
+    });
+    if (chars.containsValue(2)) {
+      twos++;
+    }
+    if (chars.containsValue(3)) {
+      threes++;
+    }
+  }
+  print ('$twos * $threes = ${twos * threes}');
+}
+
+void second() {
+  var ids = input.split('\n');
+  for (int i = 0; i < ids.length; i++) {
+    var first = ids[i];
+    for (int j = i+1; j < ids.length; j++) {
+      var second = ids[j];
+      var singleDiff = false;
+      var fi = first.runes.iterator;
+      var si = second.runes.iterator;
+      while (fi.moveNext() && si.moveNext()) {
+        if (fi.current != si.current) {
+          if (singleDiff) {
+            singleDiff = false;
+            break;
+          }
+          else {
+            singleDiff = true;
+          }
+        }
+      }
+      if (singleDiff) {
+        print('found $first $second');
+        return;
+      }
+    }
+  }
+}
